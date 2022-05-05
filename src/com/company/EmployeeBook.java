a8cab1abb8c6db8ef372655bf7956004015b7cff
gavrinol <102408582+gavrinol@users.noreply.github.com>
2022-05-05 18:39:12 +0300

    Базовая и повышенная сложности 1.2

---
+++ src/com/company/EmployeeBook.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class EmployeeBook {
+    static Employee employee1 = new Employee("Леонид Валерьевич Укупник", 3, 45033);
+    static Employee employee2 = new Employee("Валерий Герасимович Рубинштейн", 4, 29034);
+    static Employee employee3 = new Employee("Илья Вадимович Макаревич", 1, 68988);
+    static Employee employee4 = new Employee("Рудольф Николаевич Кутузов", 5, 90766);
+    static Employee employee5 = new Employee("Мария Герасимовна Овсиенко", 2, 72334);
+    static Employee employee6 = new Employee("Виктория Афанасиевна Бордюр", 3, 140990);
+    static Employee employee7 = new Employee("Григорий Кириллович Пластилинов", 5, 105900);
+    static Employee employee8 = new Employee("Марфа Ильинична Журавлева", 1, 55200);
+    static Employee employee9 = new Employee("Виктор Григорьевич Сухоруков", 4, 210500);
+    static Employee employee10 = new Employee("Лада Алексеевна Дэнс", 2, 88400);
+    static Employee[] employees = {employee1, employee2, employee3, employee4, employee5, employee6, employee7, employee8, employee9, employee10};
+
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,52 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-	Employee employee1 = new Employee("Леонид Валерьевич Укупник", 3, 45033);
-    Employee employee2 = new Employee("Валерий Герасимович Рубинштейн", 4, 29034);
-    Employee employee3 = new Employee("Илья Вадимович Макаревич", 1, 68988);
-    Employee employee4 = new Employee("Рудольф Николаевич Кутузов", 5, 90766);
-    Employee employee5 = new Employee("Мария Герасимовна Овсиенко", 2, 72334);
-    Employee employee6 = new Employee("Виктория Афанасиевна Бордюр", 3, 140990);
-    Employee employee7 = new Employee("Григорий Кириллович Пластилинов", 5, 105900);
-    Employee employee8 = new Employee("Марфа Ильинична Журавлева", 1, 55200);
-    Employee employee9 = new Employee("Виктор Григорьевич Сухоруков", 4, 210500);
-    Employee employee10 = new Employee("Лада Алексеевна Дэнс", 2, 88400);
-
-        System.out.println(employee4.getId());
-        System.out.println(employee3.getName());
-        System.out.println(employee6.getSalary());
-        employee7.setSalary(100000);
-        System.out.println(employee7.getSalary());
-        System.out.println(employee5);
+        System.out.println(EmployeeBook.employee4.getId());
+        System.out.println(EmployeeBook.employee3.getName());
+        System.out.println(EmployeeBook.employee6.getSalary());
+        EmployeeBook.employee7.setSalary(100000);
+        System.out.println(EmployeeBook.employee7.getSalary());
+        System.out.println(EmployeeBook.employee5);
         System.out.println();
 
-        Employee[] employees = new Employee[10];
-        employees[0] = employee1;
-        employees[1] = employee2;
-        employees[2] = employee3;
-        employees[3] = employee4;
-        employees[4] = employee5;
-        employees[5] = employee6;
-        employees[6] = employee7;
-        employees[7] = employee8;
-        employees[8] = employee9;
-        employees[9] = employee10;
 
-        printAllEmployees(employees);
-        printAllSalaries(employees);
-        maxSalary(employees);
-        minSalary(employees);
-        averageSalary(employees);
-        printAllNames(employees);
-        indexationOfSalaries(employees, 10);
-        minSalaryByDepartment(employees,1);
-        maxSalaryByDepartment(employees, 3);
-        indexationOfSalariesByDepartment(employees, 2, 30);
-        averageSalaryByDepartment(employees, 4);
-        printEmployeesByDepartment(employees,5);
-        printEmployeesAbove(employees, 100000);
-        printEmployeesBelow(employees, 75000);
-        maxSalary(employees);
+        printAllEmployees(EmployeeBook.employees);
+        printAllSalaries(EmployeeBook.employees);
+        maxSalary(EmployeeBook.employees);
+        minSalary(EmployeeBook.employees);
+        averageSalary(EmployeeBook.employees);
+        printAllNames(EmployeeBook.employees);
+        indexationOfSalaries(EmployeeBook.employees, 10);
+        minSalaryByDepartment(EmployeeBook.employees,1);
+        maxSalaryByDepartment(EmployeeBook.employees, 3);
+        indexationOfSalariesByDepartment(EmployeeBook.employees, 2, 30);
+        averageSalaryByDepartment(EmployeeBook.employees, 4);
+        printEmployeesByDepartment(EmployeeBook.employees,5);
+        printEmployeesAbove(EmployeeBook.employees, 100000);
+        printEmployeesBelow(EmployeeBook.employees, 75000);
+        maxSalary(EmployeeBook.employees);
 
     }
 
@@ -122,10 +100,8 @@ public class Main {
     public static void minSalaryByDepartment (Employee[] employees, int dep){
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < employees.length; i++) {
-            if (dep == employees[i].getDep()){
-                if (employees[i].getSalary() < min){
+            if (dep == employees[i].getDep() && employees[i].getSalary() < min){
                     min = employees[i].getSalary();
-                }
             }
         }
         System.out.println("Минимальная зп по отделу: " + min);
@@ -135,10 +111,8 @@ public class Main {
     public static void maxSalaryByDepartment (Employee[] employees, int dep){
         int max = 0;
         for (int i = 0; i < employees.length; i++) {
-            if (dep == employees[i].getDep()){
-                if (employees[i].getSalary() > max){
+            if (dep == employees[i].getDep() && employees[i].getSalary() > max){
                     max = employees[i].getSalary();
-                }
             }
         }
         System.out.println("Максимальная зп по отделу: " + max);
@@ -196,6 +170,4 @@ public class Main {
         }
         System.out.println();
     }
-
 }
-
